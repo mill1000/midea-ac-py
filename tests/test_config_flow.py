2265856bc8e1a436de2ce231814e2ad80b069e04
Tucker Kern <tuckkern@gmail.com>
2025-02-10 21:02:54 +0000

    Add basic options flow test

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -5,12 +5,14 @@ from unittest.mock import patch
 
 import pytest
 from homeassistant import config_entries
+from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import CONF_HOST, CONF_ID, CONF_PORT, CONF_TOKEN
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType, InvalidData
 from msmart.lan import AuthenticationError
+from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.midea_ac.const import CONF_KEY, DOMAIN
+from custom_components.midea_ac.const import CONF_BEEP, CONF_KEY, DOMAIN
 
 logging.basicConfig(level=logging.DEBUG)
 _LOGGER = logging.getLogger(__name__)
@@ -117,44 +119,50 @@ async def test_manual_flow(hass: HomeAssistant) -> None:
         assert result["errors"] == {"base": "cannot_connect"}
 
 
-# async def test_options_flow_init(hass: HomeAssistant) -> None:
-#     """Test the integration options flow works and default options are set."""
+async def test_options_flow_init(hass: HomeAssistant) -> None:
+    """Test the integration options flow works and default options are set."""
 
-#     config_entry = MockConfigEntry(
-#             domain=DOMAIN,
-#             data={
-#                 CONF_HOST: "localhost",
-#                 CONF_PORT: 6444,
-#                 CONF_ID: "1234",
-#                 CONF_TOKEN: None,
-#                 CONF_KEY: None
-#             },
-#         )
+    # Create a mock config entry
+    mock_config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            CONF_ID: "1234",
+            CONF_HOST: "localhost",
+            CONF_PORT: 6444,
+            CONF_TOKEN: None,
+            CONF_KEY: None,
+        }
+    )
 
-#     test = AsyncMock()
-#     test.id.return_value = "test"
-#     test.get_capabilities = AsyncMock()
-#     with patch("custom_components.midea_ac.AC", new=test):
-#         # instance = MockDeviceClass.return_value
-#         # _LOGGER.error(MockDeviceClass)
-#         # MockDeviceClass.id = "1234"
-#         # _LOGGER.error(MockDeviceClass.id)
-#         # Mock the property with PropertyMock
-#         # type(instance).id = PropertyMock(return_value="1234")
+    # Patch refresh and get_capabilities calls to allow integration to setup
+    with (patch("custom_components.midea_ac.config_flow.AC.get_capabilities"),
+          patch("custom_components.midea_ac.config_flow.AC.refresh")):
+        # Add mock config entry to HASS and setup integration
+        mock_config_entry.add_to_hass(hass)
+        await hass.config_entries.async_setup(mock_config_entry.entry_id)
+        await hass.async_block_till_done()
 
-#         config_entry.add_to_hass(hass)
-#         await hass.config_entries.async_setup(config_entry.entry_id)
-#         await hass.async_block_till_done()
+    assert mock_config_entry.entry_id in hass.data[DOMAIN]
+    assert mock_config_entry.state is ConfigEntryState.LOADED
 
-#     assert config_entry.entry_id in hass.data[DOMAIN]
-#     assert config_entry.state is ConfigEntryState.LOADED
+    # Show options form
+    result = await hass.config_entries.options.async_init(mock_config_entry.entry_id)
+    assert result["step_id"] == "init"
+    assert result["type"] is FlowResultType.FORM
+    assert not result["errors"]
 
-#     # # show initial form
-#     # result = await hass.config_entries.options.async_init(config_entry.entry_id)
-#     # assert "form" == result["type"]
-#     # assert "init" == result["step_id"]
-#     # assert {} == result["errors"]
-#     # # Verify multi-select options populated with configured repos.
-#     # assert {"sensor.ha_core": "HA Core"} == result["data_schema"].schema[
-#     #     "repos"
-#     # ].options
+    with patch("custom_components.midea_ac.async_setup_entry",
+               return_value=True) as mock_setup_entry:
+        result = await hass.config_entries.options.async_configure(
+            result["flow_id"],
+            user_input={
+                CONF_BEEP: False
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert mock_config_entry.options == {
+        CONF_BEEP: False,
+    }
+    assert len(mock_setup_entry.mock_calls) == 1
