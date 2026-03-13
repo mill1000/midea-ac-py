14325ccfb6b0839e5fed661ed55630783d73d5db
Tucker Kern <tuckkern@gmail.com>
2026-03-13 22:14:54 +0000

    WIP on applying overrides in __init__

--- custom_components/midea_ac/__init__.py
+++ custom_components/midea_ac/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import logging
 
+import yaml
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (CONF_HOST, CONF_ID, CONF_PORT, CONF_TOKEN,
                                  Platform)
@@ -15,12 +16,12 @@ from msmart.device import AirConditioner as AC
 from msmart.device import CommercialAirConditioner as CC
 from msmart.lan import AuthenticationError
 
-from .const import (CONF_ADDITIONAL_OPERATION_MODES, CONF_DEVICE_TYPE,
-                    CONF_ENERGY_DATA_FORMAT, CONF_ENERGY_DATA_SCALE,
-                    CONF_ENERGY_SENSOR, CONF_KEY, CONF_MAX_CONNECTION_LIFETIME,
-                    CONF_POWER_SENSOR, CONF_SHOW_ALL_PRESETS,
-                    CONF_USE_FAN_ONLY_WORKAROUND, CONF_WORKAROUNDS, DOMAIN,
-                    EnergyFormat)
+from .const import (CONF_ADDITIONAL_OPERATION_MODES, CONF_CAPABILITY_OVERRIDES,
+                    CONF_DEVICE_TYPE, CONF_ENERGY_DATA_FORMAT,
+                    CONF_ENERGY_DATA_SCALE, CONF_ENERGY_SENSOR, CONF_KEY,
+                    CONF_MAX_CONNECTION_LIFETIME, CONF_POWER_SENSOR,
+                    CONF_SHOW_ALL_PRESETS, CONF_USE_FAN_ONLY_WORKAROUND,
+                    CONF_WORKAROUNDS, DOMAIN, EnergyFormat)
 from .coordinator import MideaDeviceUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
@@ -77,6 +78,17 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     # Query device capabilities
     _LOGGER.info("Querying capabilities for device ID %s.", device.id)
     await device.get_capabilities()
+
+    # Apply capability overrides if present
+    if (yaml_input := config_entry.options.get(CONF_CAPABILITY_OVERRIDES)) is not None:
+        try:
+            overrides = yaml.safe_load(yaml_input)
+            _LOGGER.info(
+                "Applying capability overrides for device ID %s: %s", device.id, overrides)
+            device.override_capabilities(overrides)
+        except (yaml.YAMLError, ValueError) as e:
+            _LOGGER.error(
+                "Failed to apply capability overrides for device ID %s: %s", device.id, e)
 
     # Create device coordinator and fetch data
     coordinator = MideaDeviceUpdateCoordinator(hass, device)  # type: ignore
